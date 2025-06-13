4375c3ad9adf6c2fc14f0b71d61f4511041aa8d5
Dushynth <rkdushynthmm93@gmail.co>
2025-06-13 23:19:08 +0530

    added requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup,find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Dushynth",
+    author_email="rkdushynthmm93@gmail.com",
+    packages=find_packages()
+)
